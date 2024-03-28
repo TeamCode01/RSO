e5fbe925f0d2a0669a35d425d2f68b9cfd328f7a
david <delightxxls@gmail.com>
2024-03-28 12:41:42 +0300

    fix an error with submiting answer

--- rso_backend/questions/serializers.py
+++ rso_backend/questions/serializers.py
@@ -9,11 +9,11 @@ class AnswerOptionSerializer(serializers.ModelSerializer):
         model = AnswerOption
         fields = ('id', 'text', 'image')
 
-        def get_image(self, obj):
-            request = self.context.get('request')
-            if obj.image and hasattr(obj.image, 'url'):
-                return request.build_absolute_uri(obj.image.url)
-            return None
+    def get_image(self, obj):
+        request = self.context.get('request')
+        if obj.image and hasattr(obj.image, 'url'):
+            return request.build_absolute_uri(obj.image.url)
+        return None
 
 
 class QuestionSerializer(serializers.ModelSerializer):
