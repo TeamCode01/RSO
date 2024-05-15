f2a5f64a084b232e3666ae644cb4507cb136217b
David Saidov <delightxxls@gmail.com>
2024-05-15 21:06:25 +0300

    update reports (move queries to celery tasks)

--- rso_backend/reports/tasks.py
+++ rso_backend/reports/tasks.py
@@ -1,14 +1,22 @@
+import logging
 from io import BytesIO
 
 from celery import shared_task
 from openpyxl import Workbook
+from django.conf import settings
 from django.core.files.base import ContentFile
 from django.core.files.storage import default_storage
-from urllib.parse import quote, unquote
+from urllib.parse import unquote
+
+from reports.utils import get_detachment_q_results, get_safety_results, get_competition_participants_contact_data, \
+    get_competition_participants_data
+
+
+logger = logging.getLogger('tasks')
 
 
 @shared_task
-def generate_excel_file(data, headers, worksheet_title, filename, process_row_type):
+def generate_excel_file(headers, worksheet_title, filename, data_func):
     workbook = Workbook()
     worksheet = workbook.active
     worksheet.title = worksheet_title
@@ -17,46 +25,28 @@ def generate_excel_file(data, headers, worksheet_title, filename, process_row_ty
     def default_process_row(index, row):
         return [index] + list(row)
 
-    def safety_test_results_process_row(index, row):
-        return [
-            index,
-            row['region_name'] if row['region_name'] else '-',
-            f'{row["last_name"]} {row["first_name"]} '
-            f'{row["patronymic_name"] if row["patronymic_name"] else "(без отчества)"}',
-            row['detachment'] if row['detachment'] else '-',
-            row['detachment_position'] if row['detachment_position'] else '-',
-            row['attempt_number'],
-            "Да" if row['is_valid'] else "Нет",
-            row['score'],
-        ]
+    data = None
+    match data_func:
+        case 'safety_test_results':
+            data = get_safety_results()
+        case 'detachment_q_results':
+            data = get_detachment_q_results(settings.COMPETITION_ID)
+        case 'contact_data':
+            data = get_competition_participants_contact_data()
+        case 'competition_participants':
+            data = get_competition_participants_data()
 
-    def detachment_q_results_process_row(index, row):
-        return [
-            index,
-            row['region_name'],
-            row['name'],
-            row['status'],
-            row['nomination'],
-            row['participants_count'],
-            row['overall_ranking'],
-            row['places_sum'],
-            *row['places']
-        ]
-
-    process_row = default_process_row
-    if process_row_type == 'safety_test_results':
-        process_row = safety_test_results_process_row
-    elif process_row_type == 'detachment_q_results':
-        process_row = detachment_q_results_process_row
+    if not data:
+        logger.warning('Вызов функции не соответствующей кейсу для вызова функции с data')
+        return
 
     for index, row in enumerate(data, start=1):
-        processed_row = process_row(index, row)
+        processed_row = default_process_row(index, row)
         worksheet.append(processed_row)
 
     file_content = BytesIO()
     workbook.save(file_content)
     file_content.seek(0)
-
     decoded_filename = unquote(filename)
 
     content = ContentFile(file_content.read())
