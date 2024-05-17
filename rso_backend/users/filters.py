5e2dcc3dc6b4f4d68f31e6dcf5720fec75442c48
David Saidov <delightxxls@gmail.com>
2024-05-17 19:51:47 +0300

    fix rsouserfilter (add commanders to the output)

--- rso_backend/users/filters.py
+++ rso_backend/users/filters.py
@@ -1,7 +1,6 @@
 from django_filters import rest_framework as filters
-from django.db.models import Q
 from users.models import RSOUser
-
+from headquarters.models import Detachment, DistrictHeadquarter, RegionalHeadquarter, LocalHeadquarter, EducationalHeadquarter
 
 class RSOUserFilter(filters.FilterSet):
     date_of_birth = filters.DateFilter()
@@ -12,33 +11,28 @@ class RSOUserFilter(filters.FilterSet):
         field_name='date_of_birth', lookup_expr='lte'
     )
     district_headquarter__name = filters.CharFilter(
-        field_name='userdistrictheadquarterposition__headquarter__name',
-        lookup_expr='icontains',
+        method='filter_district_headquarter',
         label='Название окружного штаба'
     )
     regional_headquarter__name = filters.CharFilter(
-        field_name='userregionalheadquarterposition__headquarter__name',
-        lookup_expr='icontains',
+        method='filter_regional_headquarter',
         label='Название регионального штаба'
     )
     local_headquarter__name = filters.CharFilter(
-        field_name='userlocalheadquarterposition__headquarter__name',
-        lookup_expr='icontains',
+        method='filter_local_headquarter',
         label='Название местного штаба'
     )
     educational_headquarter__name = filters.CharFilter(
-        field_name='usereducationalheadquarterposition__headquarter__name',
-        lookup_expr='icontains',
+        method='filter_educational_headquarter',
         label='Название образовательного штаба'
     )
     detachment__name = filters.CharFilter(
-        field_name='userdetachmentposition__headquarter__name',
-        lookup_expr='icontains',
+        method='filter_detachment_headquarter',
         label='Название отряда'
     )
     region = filters.CharFilter(
         field_name='region__name',
-        lookup_expr='icontains',
+        lookup_expr='iexact',
         label='Регион'
     )
 
@@ -59,43 +53,42 @@ class RSOUserFilter(filters.FilterSet):
             'region',
         )
 
-    def filter_queryset(self, queryset):
-        queryset = super().filter_queryset(queryset)
+    def filter_district_headquarter(self, queryset, name, value):
+        headquarters = DistrictHeadquarter.objects.filter(name__iexact=value)
+        user_ids = []
+        for headquarter in headquarters:
+            user_ids.append(headquarter.commander.id)
+            user_ids.extend(headquarter.members.values_list('user_id', flat=True))
+        return queryset.filter(id__in=user_ids)
 
-        district_name = self.data.get('district_headquarter__name')
-        regional_name = self.data.get('regional_headquarter__name')
-        local_name = self.data.get('local_headquarter__name')
-        educational_name = self.data.get('educational_headquarter__name')
-        detachment_name = self.data.get('detachment__name')
+    def filter_regional_headquarter(self, queryset, name, value):
+        headquarters = RegionalHeadquarter.objects.filter(name__iexact=value)
+        user_ids = []
+        for headquarter in headquarters:
+            user_ids.append(headquarter.commander.id)
+            user_ids.extend(headquarter.members.values_list('user_id', flat=True))
+        return queryset.filter(id__in=user_ids)
 
-        if district_name:
-            queryset = queryset.filter(
-                Q(userdistrictheadquarterposition__headquarter__name__icontains=district_name) |
-                Q(userdistrictheadquarterposition__headquarter__commander__name__icontains=district_name)
-            )
+    def filter_local_headquarter(self, queryset, name, value):
+        headquarters = LocalHeadquarter.objects.filter(name__iexact=value)
+        user_ids = []
+        for headquarter in headquarters:
+            user_ids.append(headquarter.commander.id)
+            user_ids.extend(headquarter.members.values_list('user_id', flat=True))
+        return queryset.filter(id__in=user_ids)
 
-        if regional_name:
-            queryset = queryset.filter(
-                Q(userregionalheadquarterposition__headquarter__name__icontains=regional_name) |
-                Q(userregionalheadquarterposition__headquarter__commander__name__icontains=regional_name)
-            )
+    def filter_educational_headquarter(self, queryset, name, value):
+        headquarters = EducationalHeadquarter.objects.filter(name__iexact=value)
+        user_ids = []
+        for headquarter in headquarters:
+            user_ids.append(headquarter.commander.id)
+            user_ids.extend(headquarter.members.values_list('user_id', flat=True))
+        return queryset.filter(id__in=user_ids)
 
-        if local_name:
-            queryset = queryset.filter(
-                Q(userlocalheadquarterposition__headquarter__name__icontains=local_name) |
-                Q(userlocalheadquarterposition__headquarter__commander__name__icontains=local_name)
-            )
-
-        if educational_name:
-            queryset = queryset.filter(
-                Q(usereducationalheadquarterposition__headquarter__name__icontains=educational_name) |
-                Q(usereducationalheadquarterposition__headquarter__commander__name__icontains=educational_name)
-            )
-
-        if detachment_name:
-            queryset = queryset.filter(
-                Q(userdetachmentposition__headquarter__name__icontains=detachment_name) |
-                Q(userdetachmentposition__headquarter__commander__name__icontains=detachment_name)
-            )
-
-        return queryset
+    def filter_detachment_headquarter(self, queryset, name, value):
+        headquarters = Detachment.objects.filter(name__iexact=value)
+        user_ids = []
+        for headquarter in headquarters:
+            user_ids.append(headquarter.commander.id)
+            user_ids.extend(headquarter.members.values_list('user_id', flat=True))
+        return queryset.filter(id__in=user_ids)
