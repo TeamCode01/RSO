6caaaa61c6c24ee3e297805b531066d181a929be
David Saidov <delightxxls@gmail.com>
2024-08-28 22:46:04 +0300

    change entry class

--- rso_backend/rso_backend/settings.py
+++ rso_backend/rso_backend/settings.py
@@ -130,8 +130,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-# if not DEBUG:
-#     MIDDLEWARE += ['api.middlewares.CustomRequestLogsMiddleware',]
+if not DEBUG:
+    MIDDLEWARE += ['requestlogs.middleware.RequestLogsMiddleware',]
 
 ROOT_URLCONF = 'rso_backend.urls'
 
@@ -717,7 +717,7 @@ SWAGGER_SETTINGS = {
 
 REQUESTLOGS = {
     'STORAGE_CLASS': 'requestlogs.storages.LoggingStorage',
-    'ENTRY_CLASS': 'requestlogs.entries.RequestLogEntry',
+    'ENTRY_CLASS': 'api.utils.CustomRequestLogEntry',
     'SERIALIZER_CLASS': 'requestlogs.storages.BaseEntrySerializer',
     'SECRETS': ['password', 'token', 'HTTP_COOKIE', 'HTTP_X_CSRFTOKEN'],
     'ATTRIBUTE_NAME': '_requestlog',
