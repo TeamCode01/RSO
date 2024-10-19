9d99fe82b1359aabd121b1ba938ab4b016ee571b
vi.vala.vi <sovestyaaaa@gmail.com>
2024-10-19 19:22:38 +0300

    fix q4 report

--- rso_backend/reports/utils.py
+++ rso_backend/reports/utils.py
@@ -257,7 +257,7 @@ def get_safety_results():
                 row.score,
                 timestamp,
                 row.user.membership_fee,
-                row.detachment.sep_15_participants.members_number if row.detachment else '-',
+                row.detachment.sep_15_participants.first().members_number if row.detachment else '-',
             ))
         except Exception:
             pass
